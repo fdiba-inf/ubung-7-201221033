3444d9e2f1a1402980ba62593e1a2af1f6d95140
201221033 <>
2021-12-31 09:17:43 +0000

    Some message

--- src/exercise7/RecursivePrimePrinter.java
+++ src/exercise7/RecursivePrimePrinter.java
@@ -1,5 +1,4 @@
 package exercise7;
-
 import java.util.Scanner;
 
 public class RecursivePrimePrinter {
@@ -13,11 +12,25 @@ public class RecursivePrimePrinter {
     }
 
     public static void printPrimes(int n) {
-        // Implement recursive method
+        if (n == 0) {
+          return;
+        }
+        if (isPrime(n)) {
+          System.out.println(n);
+        }
+        printPrimes(n - 1);
     }
 
     public static boolean isPrime(int n) {
-        // Implement method
-        return false;
+        int divider = 2;
+        
+        while (divider < n) {
+          if (n % divider == 0) {
+            return false;
+          }
+          divider++;
+          }
+
+        return true;
     }
 }
